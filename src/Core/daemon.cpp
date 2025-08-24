d316ce1d2e19a3f25a50f0fcd444aebba0bbc741
guovsea <guovsea@gmail.com>
2025-08-24 10:41:30 +0800

    Implement daemon

---
+++ src/Core/daemon.cpp
@@ -0,0 +1,56 @@
+#include <unistd.h>
+#include "daemon.h"
+
+#include <cstring>
+#include <sys/wait.h>
+
+#include "Util/Util.h"
+#include "Log/Log.h"
+#include "Util/Config.h"
+
+namespace solar {
+
+static Logger::ptr g_logger = SOLAR_LOG_NAME("system");
+static ConfigVar<uint32_t>::ptr g_daemon_restart_interval
+    = Config::Lookup("daemon.restart_interval", (uint32_t)5, "daemon restart time");
+
+static int real_start(int argc, char* argv[], std::function<int(int argc, char* argv[])> main_cb) {
+    return main_cb(argc, argv);
+}
+static int real_daemon(int argc, char* argv[], std::function<int(int argc, char* argv[])> main_cb) {
+    ProcessInfoMgr::Instance()->parent_id = getpid();
+    ProcessInfoMgr::Instance()->parent_start_time = GetCurrentMS();
+    while (true) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            // 子进程返回
+            ProcessInfoMgr::Instance()->main_id = getpid();
+            ProcessInfoMgr::Instance()->main_start_time = GetCurrentMS();
+            SOLAR_LOG_INFO(g_logger) << "process start pid=" << getpid();
+        } else if (pid < 0) {
+            SOLAR_LOG_ERROR(g_logger) << "fork fail return=" << pid << " errno=" << errno
+                << " errstr=" << strerror(errno);
+        } else {
+            // 父进程返回
+            int status{ 0 };
+            waitpid(pid, &status, 0);
+            if (status) {
+                SOLAR_LOG_ERROR(g_logger) << "child crash pid=" << pid
+                    << " status=" << status;
+            } else {
+                // 正常退出
+                SOLAR_LOG_INFO(g_logger) << "child finished pid=" << pid;
+            }
+            ++ProcessInfoMgr::Instance()->restart_count;
+            sleep(g_daemon_restart_interval->getValue()); // 等待子进程的资源释放
+        }
+    }
+}
+
+int start_daemon(int argc, char *argv[], std::function<int(int argc, char *argv[])> main_cb, bool is_daemon) {
+    if (!is_daemon) {
+        return real_start(argc, argv, main_cb);
+    }
+    return real_daemon(argc, argv, main_cb);
+}
+}
---
+++ src/Core/daemon.h
@@ -0,0 +1,24 @@
+/**
+ * @file Daemon.h
+ * @brief 
+ * @author guovsea
+ * @email guovsea@gmail.com
+ */
+#ifndef __SOLAR_DAEMON_H__
+#define __SOLAR_DAEMON_H__
+#include <functional>
+#include "Util/Singleton.h"
+
+namespace solar {
+struct ProcessInfo {
+    pid_t parent_id = 0;
+    pid_t main_id = 0;
+    uint64_t parent_start_time = 0;
+    uint64_t main_start_time = 0;
+    uint64_t restart_count = 0;
+};
+typedef Singleton<ProcessInfo> ProcessInfoMgr;
+int start_daemon(int argc, char* argv[], std::function<int(int argc, char* argv[])> main_cb, bool is_daemon);
+}
+
+#endif //__SOLAR_DAEMON_H__
